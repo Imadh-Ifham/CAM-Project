c8454372dbe54bce7cd748c0dad72463e797cdb8
Adriel Damian <adrieldp2000@gmail.com>
2025-10-05 17:26:17 +0530

    Remove placeholder volunteer files
    Deleted dummy route and service files from the volunteer module as they are no longer needed. Actual implementations will be added separately.

--- Services/src/modules/volunteer/services/dummy.ts
+++
@@ -1,8 +0,0 @@
-// Placeholder file to ensure Git tracks this directory
-// This will be replaced with actual volunteer service implementations
-
-export class VolunteerService {
-  // Volunteer service methods will be implemented here
-}
-
-export default {};
