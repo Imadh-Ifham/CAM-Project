408e88510ab43065976261b285a7e8e7923b271c
Adriel Damian <adrieldp2000@gmail.com>
2025-10-08 23:54:03 +0530

    Suppress SafeAreaView deprecation warning in Metro config
    Added explicit platform resolver settings to metro.config.js to suppress SafeAreaView deprecation warnings from dependencies.

--- Apps/CAM-MOBILE/metro.config.js
+++ Apps/CAM-MOBILE/metro.config.js
@@ -3,4 +3,7 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Suppress SafeAreaView deprecation warning from dependencies
+config.resolver.platforms = ["ios", "android"];
+
 module.exports = withNativeWind(config, { input: "./app/globals.css" });
